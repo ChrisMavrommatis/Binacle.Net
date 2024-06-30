d52f56cf0283a730f33787e7b062abe21d9eb929
Chris Mavrommatis <56303004+ChrisMavrommatis@users.noreply.github.com>
2024-06-30 23:35:44 +0300

    added new experimental strategy

---
+++ src/Binacle.Net.Lib/Strategies/Best Fit Decreasing v1/Strategy.cs
@@ -0,0 +1,32 @@
+﻿using Binacle.Net.Lib.Abstractions.Strategies;
+using Binacle.Net.Lib.Models;
+
+namespace Binacle.Net.Lib.Strategies;
+
+internal sealed class BestFitDecreasing_v1 :
+	IBinFittingStrategy,
+	IBinFittingStrategyWithBins,
+	IBinFittingStrategyWithBinsAndItems,
+	IBinFittingOperation
+
+{
+	public IBinFittingOperation Build()
+	{
+		throw new NotImplementedException();
+	}
+
+	public BinFittingOperationResult Execute()
+	{
+		throw new NotImplementedException();
+	}
+
+	IBinFittingStrategyWithBinsAndItems IBinFittingStrategyWithBins.AndItems<TItem>(IEnumerable<TItem> items)
+	{
+		throw new NotImplementedException();
+	}
+
+	IBinFittingStrategyWithBins IBinFittingStrategy.WithBins<TBin>(IEnumerable<TBin> bins)
+	{
+		throw new NotImplementedException();
+	}
+}
