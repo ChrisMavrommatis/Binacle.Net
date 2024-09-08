6760ec15af3d716c431016b615f5543592c3c024
Chris Mavrommatis <56303004+ChrisMavrommatis@users.noreply.github.com>
2024-09-08 12:32:48 +0300

    wxperimentation with webgl

--- src/Binacle.Net.Api.UIModule/wwwroot/js/main.js
+++ src/Binacle.Net.Api.UIModule/wwwroot/js/main.js
@@ -1,15 +1,24 @@
 ﻿import WebGL from 'three/addons/capabilities/WebGL.js';
+import * as THREE from 'three';
 
-if (WebGL.isWebGL2Available()) {
-
-	// Initiate function or other initializations here
-	console.log("not")
-
-} else {
-
-	console.log("Yes")
-
-}
-
+const container = document.getElementById("renderer-container")
+const webGlAvailable = WebGL.isWebGL2Available();
 const warning = WebGL.getWebGL2ErrorMessage();
-console.log(warning);
+container.appendChild(warning);
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+cube.rotation.x += 0.01;
+cube.rotation.y += 0.01;
