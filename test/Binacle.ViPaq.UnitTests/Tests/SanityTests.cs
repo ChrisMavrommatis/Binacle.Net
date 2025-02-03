21c77f5df33dbbbe9696f596c7ef020adcce1e02
Chris Mavrommatis <56303004+ChrisMavrommatis@users.noreply.github.com>
2025-02-04 00:03:49 +0200

    added vipaq tests

---
+++ test/Binacle.ViPaq.UnitTests/Tests/SanityTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Binacle.ViPaq.UnitTests;
+
+[Trait("Sanity Tests", "Ensures the tests are configured correctly")]
+public class SanityTests
+{
+    [Fact]
+    public void Tests_Work()
+    {
+	    Assert.True(true);
+    }
+}
+
