181e165af4c60201f2676b365d2175ccaefb59a7
Chris Mavrommatis <56303004+ChrisMavrommatis@users.noreply.github.com>
2025-02-12 19:52:22 +0200

    change .NET to .Net to have the same case as everywhere

--- src/Binacle.Net.Api.Kernel/Metadata.cs
+++ src/Binacle.Net.Api.Kernel/Metadata.cs
@@ -2,7 +2,7 @@
 
 public static class Metadata
 {
-	public const string Description = "Binacle.NET is an API created to address the 3D Bin Packing Problem in real time.";
+	public const string Description = "Binacle.Net is an API created to address the 3D Bin Packing Problem in real time.";
 	public const string License = "GNU General Public License v3.0";
 	
 	public const string GitHub = "https://github.com/ChrisMavrommatis/Binacle.Net";
