a21a589d8b2a8ecca5d81d3fd2fc5e076d17612f
Chris Mavrommatis <56303004+ChrisMavrommatis@users.noreply.github.com>
2025-05-24 01:51:13 +0300

    benchmarks tooling

---
+++ config/benchmarks/README.md
@@ -0,0 +1,15 @@
+# Benchmarks
+
+Simple project containing scripts for all benchmarks
+
+
+```text
+Scaling_Fitting_FFD  = --filter *Scaling.Fitting_FFD*
+Scaling_FittingComparison = --filter *Scaling.FittingComparison*
+Scaling_Packing_FFD = --filter *Scaling.Packing_FFD*
+Scaling_PackingComparison = --filter *Scaling.PackingComparison*
+Combination_FittingSingleBaseLine = --filter *Combination.FittingSingleBaseLine*
+Combination_PackingSingleBaseLine = --filter *Combination.PackingSingleBaseLine*
+Combination_FittingMultipleBins = --filter *Combination.FittingMultipleBins*
+Combination_PackingMultipleBins = --filter *Combination.PackingMultipleBins*
+```
---
+++ config/benchmarks/run.sh
@@ -0,0 +1,23 @@
+﻿#!/bin/bash
+
+BENCH_FILE_PATH=$( realpath "$0"  )
+BENCH_FILE_DIR=$( dirname "$BENCH_FILE_PATH" )
+ROOT_DIR=$( dirname "$( dirname "$BENCH_FILE_DIR" )" )
+BENCHMARKS_PROJECT_PATH='test/Binacle.Lib.Benchmarks/'
+
+# set working directory to the root of the project
+cd "$ROOT_DIR/$BENCHMARKS_PROJECT_PATH" || exit 1
+
+echo "Running from $ROOT_DIR"
+
+# Scaling_Fitting_FFD  = --filter *Scaling.Fitting_FFD*
+# Scaling_FittingComparison = --filter *Scaling.FittingComparison*
+# Scaling_Packing_FFD = --filter *Scaling.Packing_FFD*
+# Scaling_PackingComparison = --filter *Scaling.PackingComparison*
+# Combination_FittingSingleBaseLine = --filter *Combination.FittingSingleBaseLine*
+# Combination_PackingSingleBaseLine = --filter *Combination.PackingSingleBaseLine*
+# Combination_FittingMultipleBins = --filter *Combination.FittingMultipleBins*
+# Combination_PackingMultipleBins = --filter *Combination.PackingMultipleBins*
+
+
+dotnet run -c Release --filter *Scaling.Fitting_FFD*
